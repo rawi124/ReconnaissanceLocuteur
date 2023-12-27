f06b29c914f5d31a29f760ace4faa0d3d80d0f6f
rawi124 <rawia.ben.amira1997@gmail.com>
2023-12-27 16:26:48 +0100

    edité

--- rawi/script/etp8_split_mlf.sh
+++ rawi/script/etp8_split_mlf.sh
@@ -6,6 +6,8 @@ test=14709
 test_mlf="/home/rawi/ReconnaissanceLocuteur/rawi/script/fich_sortie/phones0_test.mlf"
 train_mlf="/home/rawi/ReconnaissanceLocuteur/rawi/script/fich_sortie/phones0_train.mlf"
 
-head -n "$train" "$fichier_entree" > "$train_mlf"
-tail -n "$test" "$fichier_entree" > "$test_mlf"
+echo "#!MLF!#" > "$train_mlf" 
+echo "#!MLF!#" > "$test_mlf"
+head -n "$train" "$fichier_entree" >> "$train_mlf"
+tail -n "$test" "$fichier_entree" >> "$test_mlf"
 
